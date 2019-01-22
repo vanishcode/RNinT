23c40fb0353fd1ae6e575b3c527c8cd8ff22f401
vanishcode <vanishcode@outlook.com>
2019-01-22 19:24:42 +0800

    增加了logo

---
+++ utils/constant.go
@@ -0,0 +1,15 @@
+package utils
+
+const (
+	// Logo 字符画
+	Logo = `
+		+-+-+-+-+-+
+		|R|N|i|n|T|
+		+-+-+-+-+-+
+
+	[read novel in terminal v0.1.0]
+	`
+	// Link 项目地址
+	Link = "https://github.com/vanishcode/RNinT"
+)
+
