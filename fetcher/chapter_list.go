5891dc58aaa4130b302afbc7f2303f8ed47882bc
vanishcode <vanishcode@outlook.com>
2019-01-23 16:43:04 +0800

    增加了章节列表页面的解析；解析规则待完善

--- fetcher/chapter_list.go
+++ fetcher/chapter_list.go
@@ -1,3 +1,51 @@
 // Package fetcher 目标网站的章节列表
 package fetcher
 
+import (
+	"log"
+	"net/http"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/vanishcode/RNinT/utils"
+)
+
+// Chapter 章节抽象
+type Chapter struct {
+	Idx int
+	Title string
+	Link string
+}
+
+// ParseChapterList 章节列表请求、解析
+func ParseChapterList(url string) ([]Chapter){
+	var result []Chapter
+	var index = 0
+	u := utils.EncodeURIComponent(url)
+	client := &http.Client{}
+	reqest, err := http.NewRequest("GET", utils.BasicEngineURL + u,nil)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	reqest.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+	res, err := client.Do(reqest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	doc.Find("dd").Each(func(i int, s *goquery.Selection) {
+	  title := s.Find("a").Text()
+	  href, _ := s.Find("a").Attr("href")
+
+	  var item Chapter
+	  item.Idx = index
+	  item.Title = title
+	  item.Link = href
+	  result = append(result,item)
+
+	  index++
+	})
+	return result;
+}
--- utils/constant.go
+++ utils/constant.go
@@ -12,9 +12,14 @@ const (
 	// Link 项目地址
 	Link = "https://github.com/vanishcode/RNinT"
 
-	// 项目
-	
+	// ----------------项目----------------
+
+	// BasicEngineURL 搜索引擎地址
+	BasicEngineURL = "https://www.owllook.net"
+
 	// SearchURL 搜索结果
 	SearchURL = "https://www.owllook.net/search?wd="
+
+	// 章节URL示例 "/chapter?url=https://www.xs.la/86_86745/&novels_name=%E5%9C%A3%E5%A2%9F"
 )
 
